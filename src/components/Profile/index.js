0075865e86e93a4d40c37630e1a7b06feb5944f1
jefferson <jefferson1104junior@gmail.com>
2020-05-22 23:50:33 -0300

    About page and Profile component created

---
+++ src/components/Profile/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Jefferson Soares</h1>
+        <h2>Analista de Sistemas</h2>
+        <p>
+            Formado em analise e desenvolvimento de sistemas na Universidade Paulista, 
+            sou desenvolvedor fullstack a 3 anos, minhas habilidades são nas stacks nodeJS, 
+            reactJS e reactNative, também possuo skill com PHP e Laravel.
+        </p>
+    </div>
+)
+
+export default Profile
