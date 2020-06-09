0e3a293a0b90f66e64300f9010d09f02f1b2c82d
jefferson <jefferson1104junior@gmail.com>
2020-06-08 23:50:06 -0300

    social links

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from "styled-icons/boxicons-logos/Github"
+import { Linkedin } from "styled-icons/boxicons-logos/LinkedinSquare"
+import { Twitter } from "styled-icons/boxicons-logos/Twitter"
+import { Instagram } from "styled-icons/boxicons-logos/InstagramAlt"
+
+const Icons = {
+  Github,
+  Linkedin,
+  Twitter,
+  Instagram,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/jefferson1104/',
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/jeffersonsoares110494/',
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/jeffersonjr1104/',
+  },
+  {
+    label: 'Instagram',
+    url: 'https://www.instagram.com/jeffersonsjunior/',
+  }
+]
+
+export default links
