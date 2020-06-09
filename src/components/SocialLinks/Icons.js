ef71d69b065cd2233fe3ebb90c688a42362803f1
jefferson <jefferson1104junior@gmail.com>
2020-06-09 17:10:03 -0300

    styled components icons

--- src/components/SocialLinks/Icons.js
+++ src/components/SocialLinks/Icons.js
@@ -1,7 +1,7 @@
-import { Github } from "styled-icons/boxicons-logos/Github"
-import { Linkedin } from "styled-icons/boxicons-logos/LinkedinSquare"
-import { Twitter } from "styled-icons/boxicons-logos/Twitter"
-import { Instagram } from "styled-icons/boxicons-logos/InstagramAlt"
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
 
 const Icons = {
   Github,
