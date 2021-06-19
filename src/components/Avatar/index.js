6047e5fa936d0e40f8d990dae1c84410c12fc7e9
jefferson1104 <jefferson1104junior@gmail.com>
2021-06-19 02:26:35 -0300

    deixando responsivo

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -10,8 +10,8 @@ const Avatar = () => {
             query {
                 avatarImage: file(relativePath: {eq: "jeffersonSoares.jpg"}) {
                     childImageSharp {
-                        fixed(width: 60, height: 60) {
-                            ...GatsbyImageSharpFixed_tracedSVG
+                        fluid(maxWidth: 60) {
+                            ...GatsbyImageSharpFluid_tracedSVG
                         }
                     }
                 }
@@ -19,7 +19,7 @@ const Avatar = () => {
         `
     )
 
-    return <S.AvatarWrapper fixed={avatarImage.childImageSharp.fixed} />
+    return <S.AvatarWrapper fluid={avatarImage.childImageSharp.fluid} />
 }
 
 export default Avatar
--- src/components/recommendedPosts/styled.js
+++ src/components/recommendedPosts/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 import { Link } from "gatsby"
 
 export const RecommendedWrapper = styled.section`
@@ -17,19 +18,30 @@ export const RecommendedLink = styled(Link)`
   text-decoration: none;
   transition: background 0.5s;
   width: 50%;
+
+  ${media.lessThan("large")`
+    padding: 2rem 1rem;
+    line-height: 1.3;
+    font-size: .9rem;
+  `}
+
   &:hover {
     background: var(--borders);
   }
+
   &.previous {
     border-right: 1px solid var(--borders);
   }
+
   &.next {
     justify-content: flex-end;
   }
+
   &.previous:before {
     content: "\\2190";
     margin-right: 0.5rem;
   }
+  
   &.next:after {
     content: "\\2192";
     margin-left: 0.5rem;
